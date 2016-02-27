3ca4b31724b99a70645c32f0b7974773426f7f38
egervais7 <edward.a.gervais89@gmail.com>
2016-02-26 22:14:13 -0800

    just an initial push with a few thigns working

---
+++ index.js
@@ -0,0 +1,13 @@
+var count13 = 0;
+var count20 = 0;
+var noShow = 0;
+
+function countMar13() {
+  count13++;
+  document.getElementById("mar13Count").innerHTML = count13;
+}
+
+function countMar20() {
+  count20++;
+  document.getElementById("mar20Count").innerHTML = count20;
+}
