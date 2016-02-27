000f96473a1b12baedad4f55f32fe578ba900162
egervais7 <edward.a.gervais89@gmail.com>
2016-02-27 00:11:17 -0800

    added localStorage for votes

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
   console.log("ready");
-  var count13 = 0;
-  var count20 = 0;
-  var noShow = 0;
+
+  if (localStorage.voteCount13) {
+    var count13 = JSON.parse(localStorage.getItem('voteCount13'));
+    $("#mar13Count").html(count13);
+  } else {
+    var count13 = 0;
+  }
+
+  if (localStorage.voteCount20) {
+    var count20 = JSON.parse(localStorage.getItem('voteCount20'));
+    $("#mar20Count").html(count20);
+  } else {
+    var count20 = 0;
+  }
+
+  if (localStorage.voteCountNoShow) {
+    var noShow = JSON.parse(localStorage.getItem('voteCountNoShow'));
+    $("#noShowCount").html(noShow);
+  } else {
+    var noShow = 0;
+  }
+
   var clicks = 0;
 
   var countMar13 = function() {
@@ -13,6 +32,7 @@ $(document).ready(function() {
         $("#mar13Count").html(count13);
       }
     });
+    localStorage.setItem('voteCount13', JSON.stringify(count13));
   };
 
   countMar13();
@@ -25,6 +45,7 @@ $(document).ready(function() {
         $("#mar20Count").html(count20);
       }
     });
+    localStorage.setItem('voteCount20', JSON.stringify(count20));
   };
 
   countMar20();
@@ -37,6 +58,7 @@ $(document).ready(function() {
         $("#noShowCount").html(noShow);
       }
     });
+    localStorage.setItem('voteCountNoShow', JSON.stringify(noShow));
   };
 
   noShowFunc();
