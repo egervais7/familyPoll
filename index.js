405215ac4c9cff7b600840102fe8fbd4bf888750
egervais7 <edward.a.gervais89@gmail.com>
2016-02-26 22:43:18 -0800

    a few updates, still working on getting index .js page to connect and css

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var count13 = 0;
-var count20 = 0;
-var noShow = 0;
+$(document).ready(function() {
+  console.log("ready");
+  var count13 = 0;
+  var count20 = 0;
+  var noShow = 0;
 
-function countMar13() {
-  count13++;
-  document.getElementById("mar13Count").innerHTML = count13;
-}
+  var countMar13 = function() {
+    count13++;
+    document.getElementById("mar13Count").innerHTML = count13;
+  };
 
-function countMar20() {
-  count20++;
-  document.getElementById("mar20Count").innerHTML = count20;
-}
+  var countMar20 = function() {
+    count20++;
+    document.getElementById("mar20Count").innerHTML = count20;
+  };
+
+  var noShowCount = function() {
+    noShow++;
+    document.getElementById("noShowCount").innerHTML = noShow;
+  };
+
+});
